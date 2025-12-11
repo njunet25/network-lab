c975e8abbc086e10a66d7f9a5494ae87fe5dea15
pomoke <pomoke@typeof.pw>
2025-12-11 08:54:54 +0800

    Stop-and-Wait code

--- src/tftp-loop.c
+++ src/tftp-loop.c
@@ -1,4 +1,5 @@
 #include <arpa/inet.h>
+#include <asm-generic/errno-base.h>
 #include <netinet/in.h>
 #include <netinet/ip_icmp.h>
 #include <arpa/tftp.h>
@@ -7,6 +8,7 @@
 #include <stdio.h>
 #include <string.h>
 #include <sys/socket.h>
+#include <sys/ucontext.h>
 #include <unistd.h>
 #include <stdlib.h>
 #include <netdb.h>
@@ -24,6 +26,56 @@
   @return non-zero on success.
  */
 int tftp_loop(int sfd, FILE *fp, short window) {
+  // We don't know if this code is correct.
 
-  return 1;
+  int ret, acked = 0;
+  size_t block_size = 512;
+  char *buf = malloc(1024);
+  char *block;
+  int len;
+  int retrys = 0;
+  int extra_ack = 0;
+
+  do  {
+    ret = recv(sfd, buf, 1024, 0);
+    if (ret < 0 && errno == EAGAIN) {
+      if (++retrys > 10) {
+        fprintf(stderr, "bailing out...\n");
+        exit(1);
+      }
+      goto ack;
+    } else if (ret < 0) {
+      perror("recv");
+      return 1;
+    }
+    retrys = 0;
+
+    ret = tftp_parse_response(buf, ret, &block, &block_size);
+    if (ret < 0) {
+      fprintf(stderr, "unexpected error %d: %s\n", -ret, block);
+      return 1;
+    }
+    
+    if (ret == acked + 1) {
+      acked++;
+      fprintf(stderr, "write block %d, size %ld\n", acked, block_size);
+        ret = fwrite(block,block_size,1, fp);
+        if (ret < 0) {
+          perror("fwrite");
+          return 1;
+        }
+    } 
+    // `atftp` sends an extra ACK on first duplicate DATA, ignore further extra ACKs.
+    // `tftp-hpa` always sends current ACK.
+
+ack:
+    len = tftp_build_ack(buf, 1024, acked);
+    ret = send(sfd, buf, len, 0);
+    if (ret < 0) {
+      perror("send");
+      return 1;
+    }
+  } while (block_size == 512);
+  free(buf);
+  return 0;
 }
